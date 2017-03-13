35694346f121a792da223f45573cc5e587da59a9
qianqing <qian.qing@aliyun.com>
2017-03-13 12:02:56 +0800

    1.0.0

---
+++ template/src/assets/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qianqing on 16/9/9.
+ */
+export default function() {
+    return {
+
+    };
+};
---
+++ template/src/assets/js/router.js
@@ -0,0 +1,43 @@
+// 页面路由函数集，支持vue-router和原生url
+
+/***
+ * 页面跳转
+ * @param url
+ * @param $router
+ * @param isReplace
+ */
+export function go (url, $router = null, isReplace = false) {
+  if (/^javas/.test(url) || !url) {
+    return;
+  }
+
+  let useRouter = typeof url === 'object' || ($router && typeof url === 'string' && !/http/.test(url))
+  if (useRouter) {
+    if (!isReplace) {
+      $router.push(url);
+    } else {
+      $router.replace(url);
+    }
+  } else {
+    if (!isReplace) {
+      window.location.href = url;
+    } else {
+      window.location.replace(url);
+    }
+
+  }
+}
+
+/***
+ * 获取当前URL
+ * @param url
+ * @param $router
+ * @returns string
+ */
+export function getUrl (url, $router) {
+  // Make sure the href is right in hash mode
+  if ($router && !$router._history && typeof url === 'string' && !/http/.test(url)) {
+    return `#!${url}`
+  }
+  return url && typeof url !== 'object' ? url : 'javascript:void(0);'
+}
