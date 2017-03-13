d77b0edc6f8647691934571a4a6c5fc4456b9137
qianqing <qian.qing@aliyun.com>
2017-03-13 13:00:52 +0800

    1.0.0

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    {{#router}}
+    <img src="./assets/images/logo.png">
     <router-view></router-view>
-    {{else}}
-    <hello></hello>
-    {{/router}}
   </div>
 </template>
 
 <script>
-{{#unless router}}
-import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
-{{/unless}}
 export default {
-  name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
-  components: {
-    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
+  name: 'app'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
